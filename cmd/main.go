47f055d7b6ea4e2263b273f6156493c43dfa4e41
Prashanta Chakraborty <86629940+prashanta0234@users.noreply.github.com>
2025-08-06 18:03:33 +0600

    added main file

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+
+	router.Use(gin.Recovery())
+
+	router.Run(":8080")
+}
